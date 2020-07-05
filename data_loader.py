54288f8b97541a124b0039804eaa8822e86d872e
Ofek <ofekp@users.noreply.github.com>
2020-07-05 18:02:53 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -10,7 +10,7 @@ class ObjectDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms=None):
         self.root = root
         self.transforms = transforms
-        # load all image files, sorting them to
+        # load all imiage files, sorting them to
         # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(os.path.join(root, "images"))))
         tags_dict = {}
@@ -64,8 +64,6 @@ class ObjectDataset(torch.utils.data.Dataset):
         image_id = torch.tensor([idx])
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         # suppose all instances are not crowd
-        # (ofekp) "crowd" naming should be changed everywhere in this file,
-        #         it is probably based on code from this post https://github.com/pytorch/vision/issues/1700
         iscrowd = torch.zeros((num_anns,), dtype=torch.int64)
 
         target = {}
